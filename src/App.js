0f350717a0f4175f4ad06fa0c62de4fd23472c27
mrinalb9 <mrinalb1064@gmail.com>
2022-09-23 00:20:12 +0530

    Update 1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
 import logo from './logo.svg';
 import './App.css';
+import './fonts/Nabla-Regular/Nabla-Regular.ttf'
+import './index.css';
+import React, {useState} from 'react';
+
+
+const quotes = [
+    "Old is Gold.", 
+    "Nature relives through oneself!", 
+    "They shall not give up",
+    "Hello there",
+    "Wow"
+    ]
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [quote, setQuote] = useState(quotes[0])
+
+    function generate () {
+        const randomQuote = quotes[Math.floor(Math.random()*quotes.length)]
+        setQuote(randomQuote)
+
+    }
+
+    return(
+        <div>
+        <div className='quotes'>
+            <span className="fade-in">
+                 {quote}
+            </span>
+        </div>
+        <div className="text">
+            <button className='button' onClick={generate} >Generate Random Quote</button>
+        </div>
+        </div>
+    )
 }
 
 export default App;
